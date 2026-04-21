b5ad1838d14661a0b4fc43c573d55558b190d610
kgmyatthu <2962000kaungmyatthu@gmail.com>
2026-04-22 03:01:50 +0700

    fix(giscus): move comment input to top so users don't need to scroll

--- src/components/DiscussionPage.tsx
+++ src/components/DiscussionPage.tsx
@@ -33,7 +33,7 @@ export function DiscussionPage({ data, onNavigate }: DiscussionPageProps) {
     script.setAttribute('data-strict', '1');
     script.setAttribute('data-reactions-enabled', '1');
     script.setAttribute('data-emit-metadata', '0');
-    script.setAttribute('data-input-position', 'bottom');
+    script.setAttribute('data-input-position', 'top');
     script.setAttribute('data-theme', 'gruvbox');
     script.setAttribute('data-lang', 'en');
     script.crossOrigin = 'anonymous';
